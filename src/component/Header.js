37b5c1fbcb3a212aa79f4a7345af0695dd6915ec
athiragi <athirasnair72476@gmail.com>
2022-05-17 11:26:42 +0530

    navbar completed

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Addstudent from './Addstudent'
 
 const Header = () => {
   return (
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Mount Zion College </a>
+      <Link to="/" className="navbar-brand">Mount Zion College</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Add Student</a>
-        <a className="nav-link" href="#">Search Student</a>
+          <Link className="nav-link active" to="/">Add Student</Link>
+          <Link className="nav-link active" to="/search">Search Student</Link>
         
       </div>
     </div>
